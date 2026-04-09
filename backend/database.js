01a0432774e1542aa51b48e9d218329d699b708f
ribbinzcat-afk <ribbinzcat@gmail.com>
2026-04-09 22:51:36 +0700

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,9 @@
-import Database from "better-sqlite3";
+fileURLTfileURLToPath Database from "better-sqlite3";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import fs from "fs";
 
-const __dirname = dirname(fileURLTfileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // --- ปรับปรุงส่วนการเลือก Path ของ Database ---
 let dbPath;
