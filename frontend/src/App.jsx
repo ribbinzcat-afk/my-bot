3902031b621a97ef1c4d170805d41c464ed2de35
ribbinzcat-afk <ribbinzcat@gmail.com>
2026-04-09 12:03:13 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Sidebar from "./components/Sidebar.jsx";
 
 // Axios defaults
 const API = axios.create({
-  baseURL: "https://discord-ai-bot-tvv7.onrender.com/api" || "/api",
+  baseURL: "https://discord-ai-bot-tvv7.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
